f3044183ec950e98d0ed83e688207bf0fadee951
npochkhua19-ux <npochkhua19@gmail.com>
2026-04-14 13:39:32 -0400

    Add files via upload

---
+++ level 019/exam.py
@@ -0,0 +1,101 @@
+#1pythonი არის პროგრამირების ენა
+#2იმიტომ რომ ადვილია მრავალფეროვანია და ბევრ რამეში შეგვიძლია გამოვიყენონ
+#3 visual studio codeში და phycharmში
+#4პრინტს გამოაქ გამოსახულება ტერმინალზე
+#5
+print("hello world")
+#6string ი არის ტექსტი
+#7 "" ამ პრჭყალებში იწერება
+#8
+a="hello world"
+b="tbilisi"
+c="georgia"
+#9ინტეგერი არის რიცხვი
+#10
+e=12
+f=32
+g=4
+#11 stringi aris teqsti xolo integeri aris ricxvi
+#12 phythons
+#13 10 s gamoitans
+#14 10s gamoitans
+#15 print(10)aris intejer da print("10") es aris stringi 
+#17pirvel rigshi varqmevts saxels cvlad shemdeg vwert viyenebt tolobis nishans da vwert cvladshi rasac gvinda
+#18
+name="nika"
+age=15
+city="georgia"
+#19
+name="nika"
+print(name)
+#20
+age=15
+print(age)
+#21 toloba aris miniwebis operatori
+#22 pirveli xazi aris cvladis sheqmna meore xazi aris am cvladis gamotana
+#23 sheidzleba cvladis saxelsshi iyos ricxvi radgan pythoni agiqvams ricxvsac
+#24 b c 
+#25 
+user_name=23
+saxeli=43
+clas=9
+age=15
+city="tbilisi"
+#26 inputi gamiyeneba momxmareblis informaciis misagebad
+#27
+k=input("sheiyvane sheni saxeli:")
+#28
+h=input("sheiyvane sheni saxeli:")
+print(h)
+#29 imas rasac davwert terminalshi
+#30 
+t=input("sheiyvane sheni asaki:")
+#31 phythonshi komnari aris roca ragacis axsna ginda iyeneb komentars rom sxva romgamoiyenebs am kods ar daibnes da saqme gaumartivdes
+#32 # iwyeba
+#33
+#gamarjona
+#hello world
+#georgia
+#34 sityva weria arasworad print unda eweros
+#35 prwyali aklia
+#36 prwyalebi aklia
+#37
+print("nikoloz")
+print("pochkhua")
+print(15)
+#38
+name="nikoloz"
+surname="pochkhua"
+age=15
+print(name,surname,age)
+#39 case sensitive არის მგრძნობიარე ენა როცა დიდად დაწერილ ასოზე ერორს აგდებს
+#40 არ არის რადგან პირველი არის დიდ ასოტი დაწყები მეორე არუს სულ  uper caseით დაწერილი და სულპირველი კი პატარა შრიფთით არის დაწერილი
+#41 5 იანს
+#42 5 იანს
+#43 ერთი არის სტრინგი მეორე ინტეჯერი
+#44 
+colour="black"
+print(colour)
+#45 
+w=input("enter your name: ")
+print("hello"," ",w)
+#46
+name=input("sheiyvane sheni saxeli: ")
+age=input("sheiyvane sheni asaki: ")
+country=input("sheiyvane qveyana : ")
+hobby=input("sheiyvane sheni hobb: ")
+print(name," ",age," ",country," ",hobby)
+#47 moxmarebels shmoyavs sa xeli shemdeg iprinteba welcome da shemdeg iprinteba moxmareblis saxeli
+#48 
+print("enter your name")
+Name=input("enter your name: ")
+print("nice to meet you"," ",Name)
+#49 ჩვენ შესავალში ვისწავლეთ printi ცვლადების შექმნა inputი ლისტები ბულეანი დაინტეჯერ სტრინგი და ფლოათი
+#50
+o=input("sheiyvane sheni saxeli: ")
+p=input("sheiyvane sheni asaki: ")
+print("hello"," ",o)
+#es programa motxovs momxmarebels saxels da asaks shemdeg ki mas miesalmeba
+
+
+
