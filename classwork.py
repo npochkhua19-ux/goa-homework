be7ed69ae5053d575c3f0df757c31de63a366a3a
npochkhua19-ux <npochkhua19@gmail.com>
2026-04-24 12:37:26 -0400

    Add files via upload

---
+++ classwork.py
@@ -0,0 +1,36 @@
+# # class work
+
+# 1) ჩამოაყალიბე ყველა exceptions-ის ტიპი
+
+# 2) დაწერე თითოეულის მაგალითი
+
+# 3) დაწერე მარტივი კალკულატორი აქამდე ნასწავლი მასალით ინფუთებითა და იფელსის დახმარებით ყველანაირი შეცდომის გაერეშე
+# typeerror, nameerror,syntaxerror,valueerror,indexerror
+# typeerror
+# print(5+"hello world")
+# # NameError
+# saxeli="nikoloz"
+# print(name)
+# # syntaxerror
+# a=0
+# if a=0
+#  print("hello")
+# # valueerror
+# age="oci"
+# age=int(age)
+# # indexerror
+# fruits=["apple","banana"]
+# print(fruits[5])
+# kalkulatori
+a=int(input("sheiyvane pivreli ricxvi: "))
+c=input('sheiyvane matematikuri moqmendeba: ')
+b=int(input("sheiyvane meore ricxvi"))
+if c== "+":
+ print("a+b")
+elif c=="-":
+ print(a-b)
+elif c =="*":
+ print(a*b)
+elif c=="/":
+ print(a/b)
+ 
