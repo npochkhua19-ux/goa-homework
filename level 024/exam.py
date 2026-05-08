8266dc62182d0e7810548cb55580a9110706e46d
npochkhua19-ux <npochkhua19@gmail.com>
2026-05-08 20:42:47 +0400

    Add files via upload

---
+++ level 024/exam.py
@@ -0,0 +1,56 @@
+# # 1
+# print("hello world")
+# # 2
+# a=input("sheiyvane sheni saxeli:")
+# print("gamarjoba" + " " + a)
+# # 3
+# b=input("sheiyvaen sheni asaki: ")
+# print("shen xar" + " " +b+" "+"wlis")
+# # 4
+# c=int(input("sheiyvane ricxvi"))
+# d=int(input("sheiyven ricxvi"))
+# print(c+d)
+# print(c*d)
+# # 5
+# for i in range(31):
+#     print(i)
+# # 6
+# i=20
+# while i>= 1:
+#     print(i)
+#     i-= 1
+# # 7
+# number=int(input("sheiyvane ricxvi:"))
+# if number%2==0 :
+#     print("luwia")
+# else:
+#     print("kentia")
+# # 8
+# jami=0
+# for i in range(1,101):
+#     jami=jami + i
+# print(jami)
+# # 9
+# list=["vashli","msxali","fortoxali","atami",'feixo']
+# print(list[0])
+# print(list[4])
+# # 10
+# for i in list:
+#     print(i)
+# # 11
+# city='tbilisi'
+# # 12
+# name='nikoloz'
+# surname='pochkhua'
+# age=15
+# print(name,surname,age)
+# # 13
+# h=float(input('sheiyvaen ricxvi:'))
+# j=float(input("sheiyvane meore ricxvi:"))
+# print(h%j)
+# # 14
+# tuple=("vashli","msxali","fortoxali","atami",'feixo')
+# print(tuple[1])
+# 15
+person={"name":"giorgi","age":"48","city":"batumi"}
+print(person["city"])
