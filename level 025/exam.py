5da156f3bd53475a65b0e3d3ebad6737619507d6
npochkhua19-ux <npochkhua19@gmail.com>
2026-05-12 21:58:08 +0400

    Add files via upload

---
+++ level 025/exam.py
@@ -0,0 +1,56 @@
+# 1
+print("my first phython program ")
+# 2
+a=input("sheiyvane sheni saxeli :")
+b=input("sheiyvane sheni gvari :")
+print(a+" "+b)
+# 3
+favorite_food="mwvadi"
+# 4
+c=int(input("sheiyvane pirveli ricxi :"))
+d=int(input("sheiyvane meore ricxvi :"))
+print(c+d)
+print(c-d)
+print(c*d)
+# 5
+for i in range(20):
+    if i % 2==0:
+        print(i)
+# 6
+f=1
+while f <=15:
+    print(f)
+    f+= 1
+# 7
+h=int(input("sheiyvane sheni asaki :"))
+if h >=18:
+    print("სრულწლოვანი")
+else:
+    print("არასრულწლოვანი")
+# 8
+g= 0
+for i in range(1,31):
+    g += i
+print(g)
+# 9
+list=["msxali","marwyvi" ,"atami","alubali","vashli","gargari"]
+print(list[4])
+# 10
+list=["msxali","marwyvi" ,"atami","alubali","vashli","gargari"]
+for i in list:
+    print(i)
+# 11
+tuple=("msxali","marwyvi" ,"atami","alubali","vashli","gargari")
+print(tuple[-1])
+# 12
+dictionary= {"name":"nikoloz","surname":"pochkhua","age":15}
+print(dictionary["surname"])
+# 13
+j=int(input("sheiyvane ricxv :"))
+print(j ** 2)
+# 14
+st="gamarjoba"
+print(len(st))
+# 15
+for i in range(10,0,-1):
+    print(i)
