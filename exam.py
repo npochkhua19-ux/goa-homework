59ed86cc5373cc185331bd9f7bb0dba2ff8eb5f8
npochkhua19-ux <npochkhua19@gmail.com>
2026-06-02 21:29:25 +0400

    Add files via upload

---
+++ exam.py
@@ -0,0 +1,57 @@
+# # 1 variable aris monacemta yuti is inaxavs onacemebs
+# # 2 int aris ricxvebi da str aris asoebi 
+# # 3 inputi aris brdzaneb ris daxmarebitac momxmarebels sheudzlia sheiyvanos tavisi informacia
+# # 4 listi aris aris monacemta sia
+# # praqtikuli davaleba
+# # 1
+# a=input("sheiyvaen shenisaxeli : ")
+# print("gamarjoba"," ",a)
+# # 2
+# b=int(input("sheiyvane ricxvi: "))
+# c=int(input("sheiyvane ricxvi: "))
+# print(b+c,b-c,b*c)
+# # 3
+# for i in range(1,51):
+#     if i % 2 ==0:
+#         print(i)
+# # 4
+# d=int(input("sheiyvane ricxvi :"))
+# if d>0:
+#     print("dadebitia")
+# elif d<0:
+#     print("uaryofitia")
+# else:
+#     print("nulia")
+# # 5
+# f=0
+# for i in range(1,101):
+#     f=f+i
+#     print(f)
+#     # 6
+# my_list=[10,20,30,40,50,60,70,80,90,100]
+# print(my_list[0],my_list[9],len(my_list))
+# # 7
+# y=input('sheiyvane sityva :')
+# print(len(y))
+# # 8
+# numbers=[10,20,30,40,50,60,70,80,90,100]
+# max_num=numbers[0]
+# for n in numbers:
+#     if n > max_num:
+#         max_num=n
+# print(max_num)
+# 9
+e=int(input("sheiyvane ricxvi: "))
+r=int(input("sheiyvane ricxvi: "))
+u=int(input("sheiyvane ricxvi: "))
+p=int(input("sheiyvane ricxvi: "))
+o=int(input("sheiyvane ricxvi: "))
+m=e+r+u+p+o
+w=m/5
+print(w)
+# 10
+l=(input("sheiyvaen paroli:"))
+if l == "admin123":
+    print("წვდომა დასვებულია")
+else:
+    print("არასწორია პაროლი")
